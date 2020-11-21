6c90c15dd00645b74e18bf60eb172075a8c430ca
hank <hank8451@gmail.com>
2020-11-21 17:18:16 +0800

    add not default static folder to public

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    # static_folder= "static",   # 靜態檔案的資料夾名稱（預設）
+    # static_url_path= "/static" # 靜態檔案對應的（預設
+    static_folder = "public",
+    static_url_path = "/"
+)
 
 @app.route("/")
 def home():
