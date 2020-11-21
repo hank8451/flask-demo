484f85f24a4c0ef4a94a0e594076a0bc367a34cb
hank <hank8451@gmail.com>
2020-11-21 16:31:30 +0800

    add dynamic route /user/<user_name>

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def test():
     return "I am a test"
 
+@app.route("/user/<user_name>")
+def log_in(user_name):
+    return "Hello " + user_name
+
 if __name__ == "__main__":
     app.run()
